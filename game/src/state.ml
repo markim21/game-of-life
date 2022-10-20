a1b2b0014d1ff9043c0ce39ea1b510d9bd4febf8
Marya Kim <mnk45@cornell.edu>
2022-10-20 12:22:14 -0400

    Implement evaluation of neighboring squares. Change square representation.

--- game/src/state.ml
+++ game/src/state.ml
@@ -3,10 +3,14 @@ open Graphics
 
 (* Define the components of a grid. *)
 module Grid = struct 
-  type square = 
-  | Alive
-  | Dead 
+  (* every cell stores its location and alive status. *)
+  type square = {
+    x : int;
+    y : int;
+    alive : bool;
+  }
 
+  (* every grid has x by y dimensions and a 2D array of squares *)
   type grid = {
     x : int; 
     y : int;
@@ -16,51 +20,60 @@ end
 
 
 (* Define the possible neighboring coordinates of a block *)
-module Coordinates = struct 
-  let am1 (a : int) : int = a - 1
-  let amp (a: int) : int = a
-  let ap1 (a : int) : int = a + 1
-  let iter = [
-  (am1, am1); (amp, am1); (ap1, am1); 
-  (am1, amp); (ap1, amp); 
-  (am1, ap1); (amp, ap1); (ap1, ap1); 
-]
-end 
+
+(* a = the current location. 
+  if a__ is outside the grid bounds,
+    then do not add that block to the list of neighbors.
+  if a__ is inside the grid bounds, 
+    then add that block to the list of neighbors. *)
+let ap1 (a : int) : int = a + 1
+let am1 (a : int) : int = a - 1 
+let amp (a : int) : int = a
+
+let coordinates = 
+  [ (am1, ap1); (amp, ap1); (ap1, am1);
+    (am1, amp); (ap1, amp);
+    (am1, ap1); (amp, ap1); (ap1, ap1)]
+
+(* helper function to test if coordinate is within grid dimensions *)
+let validate_boundary (max : int) (min : int) (value : int) = 
+  (value < max) && (value > min)
 
 
-(*For a given block, return a list of the coordinates for neighboring blocks*)
-let rec get_neighbors (block : (int * int)) (coords : ((int -> int) * (int -> int)) list ) (grid : Grid.grid) = 
+(*For a given block, return a list of the neighboring squares
+   Filter invalid coordinates - ie if the block is a edge piece. *)
+let rec get_neighbor_coordinates (square : Grid.square) (g : Grid.grid) (coords : ((int -> int) * (int -> int)) list ) (neighbors: Grid.square list) : Grid.square list = 
+  (* iterate through every coordinate in coordinates list. *)
   match coords with 
-  | [] -> [] 
-  | (a,b) :: t -> 
-    match block with
-    | (x,y) -> 
-      if (a x < grid.x) && (a x >= 0) && (b y < grid.y) && (b y >=0) 
-        then (a,b) :: get_neighbors block t grid 
-      else get_neighbors block t grid
+  | [] -> neighbors 
+  | (x,y) :: t -> 
+    (* define x y = coordinates of the potential neighbor square we're checking *)
+    let x = x square.x in let y = y square.y in 
 
-
-(*For a list of block coordinates in a grid, return the number of alive*)
-let rec get_live_count grid coords = 
-  match coords with 
-  | [] -> 0
-  | (a,b) :: t -> 
-    if grid.(a).(b) = Alive then 1 + get_live_count grid t else get_live_count grid t
-
-
-(* For a given block in a grid, get number of its alive neighbors *)
-let get_live_neighbor_count block grid = 
-  let neighbors = get_neighbors block Coordinates.iter grid in 
-    get_live_count grid neighbors 
-
-
-(* For each block in a grid, determine the number of alive neighbors *)
+      if validate_boundary 0 g.x x && validate_boundary 0 g.y y
+        then get_neighbor_coordinates square g t (( g.squares.(x).(y) ) :: neighbors)
+        else get_neighbor_coordinates square g t (neighbors)
 
 
 
-(* For the number of alive neighbors for each block, 
-  and the block's current state,
-  generate the next generation's state of alive/dead *)
+(* For a list of squares, count the number of neighbors alive tail recursively *)
+let rec count_living_tr (lst : Grid.square list) (count : int) : int = 
+  match lst with 
+  | [] -> count
+  | x :: t -> if x.alive then count_living_tr t (count+1) else count_living_tr t count
 
-let get_new_generation (grid : Grid.grid) = 
+let get_live_count (lst : Grid.square list) : int = 
+  count_living_tr lst 0
+
+
+
+(* For a given square, determine the square's next state
+   based on the square's neighbors. 
+      1. get the square's neighbors
+      2. get how many of its neighbors are alive
+      3. if square is alive --> <game of life rules>
+      4. return new state as boolean  *)
+
+
+(* Evaluate the new state for every block in a given grid. *)
 
