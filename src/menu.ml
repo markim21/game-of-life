ed2fb3f9229d4514124ac02808e9d2894cddc484
cindyzixin <zixincindy.huang@gmail.com>
2022-12-06 16:48:15 -0500

    Complete patterns second page
    - deleted set font size line; reoriented all texts
    
    - users can switch between the 2 patterns page

--- src/menu.ml
+++ src/menu.ml
@@ -1,5 +1,8 @@
 open Graphics
 
+let pink = rgb 247 208 209
+let light_blue = rgb 86 154 196
+
 let make_screen (width : int) (height : int) =
   let size = " " ^ string_of_int width ^ "x" ^ string_of_int height in
   open_graph size;
@@ -21,40 +24,41 @@ let rect_outline (c : color) x y w h line =
 let write_word (c : color) size x y word =
   moveto x y;
   set_color c;
-  set_font ("-*-*-medium-r-*--" ^ string_of_int size ^ "-*-*-*-*-*-iso8859-*");
+  (* set_font ("-*-*-medium-r-*--" ^ string_of_int size ^
+     "-*-*-*-*-*-iso8859-*"); *)
   draw_string word
 
 let start_menu () = make_screen 1000 1000
 
 let menu_draw () =
   set_background_color white;
-  write_word black 50 360 700 "Game of Life";
-  write_word black 40 250 630 "Start by Choosing Your Grid";
-  write_word black 30 320 580 "Press 'e' to exit the game";
+  write_word black 50 470 700 "Game of Life";
+  write_word black 40 425 630 "Start by Choosing Your Grid";
+  write_word black 30 430 580 "Press 'e' to exit the game";
 
   rect_outline black 180 490 250 60 1;
-  write_word black 50 245 495 "3 x 3";
+  write_word black 50 285 510 "3 x 3";
 
   rect_outline black 180 390 250 60 1;
-  write_word black 50 245 395 "5 x 5";
+  write_word black 50 285 410 "5 x 5";
 
   rect_outline black 180 290 250 60 1;
-  write_word black 50 220 295 "10 x 10";
+  write_word black 50 280 310 "10 x 10";
 
   rect_outline black 580 490 250 60 1;
-  write_word black 50 620 495 "20 x 20";
+  write_word black 50 680 510 "20 x 20";
 
   rect_outline black 580 390 250 60 1;
-  write_word black 50 620 395 "50 x 50";
+  write_word black 50 680 410 "50 x 50";
 
   rect_outline black 580 290 250 60 1;
-  write_word black 50 600 295 "100 x 100";
+  write_word black 50 675 310 "100 x 100";
 
   rect_outline black 410 180 200 60 1;
-  write_word black 50 450 190 "Rules";
+  write_word black 50 495 200 "Rules";
 
   rect_outline black 385 100 250 60 1;
-  write_word black 50 415 105 "Patterns"
+  write_word black 50 485 120 "Patterns"
 
 let ocaml =
   [|
@@ -216,17 +220,17 @@ let rec menu_switch () =
 
 let rules_draw () =
   clear_graph ();
-  write_word black 50 450 700 "Rules";
-  write_word black 30 75 620
+  write_word black 50 490 700 "Rules";
+  write_word black 30 300 620
     "1. Any live cell with two or three live neighbours survives.";
-  write_word black 30 75 540
+  write_word black 30 300 540
     "2. Any dead cell with three live neighbours becomes a live cell.";
-  write_word black 30 75 460
+  write_word black 30 300 460
     "3. All other live cells die in the next generation.";
-  write_word black 30 75 380
+  write_word black 30 300 380
     "4. All other dead cells stay dead in the next generation.";
-  write_word red 40 260 280 "Press 'e' to exit the game";
-  write_word red 40 120 200 "Press 'space' to go back to the main menu";
+  write_word red 40 430 280 "Press 'e' to exit the game";
+  write_word red 40 390 200 "Press 'space' to go back to the main menu";
   menu_switch ()
 
 let glider () =
@@ -337,9 +341,173 @@ let block () =
   rect_outline blue 740 600 30 30 3;
   write_word black 30 820 600 "block"
 
-let patterns_draw () =
+let unnamed_1 () =
+  rect_block pink 150 600 30 30;
+  rect_outline light_blue 150 600 30 30 3;
+
+  rect_block pink 180 630 30 30;
+  rect_outline light_blue 180 630 30 30 3;
+
+  rect_block pink 210 630 30 30;
+  rect_outline light_blue 210 630 30 30 3;
+
+  rect_block pink 240 630 30 30;
+  rect_outline light_blue 240 630 30 30 3;
+
+  rect_block pink 240 600 30 30;
+  rect_outline light_blue 240 600 30 30 3;
+
+  rect_block pink 210 570 30 30;
+  rect_outline light_blue 210 570 30 30 3;
+
+  rect_block pink 180 570 30 30;
+  rect_outline light_blue 180 570 30 30 3;
+
+  rect_outline light_blue 180 600 30 30 3;
+  rect_outline light_blue 210 600 30 30 3;
+
+  write_word black 30 290 610 "unnamed"
+
+let unnamed_2 () =
+  rect_block pink 180 500 30 30;
+  rect_outline light_blue 180 500 30 30 3;
+
+  rect_block pink 180 470 30 30;
+  rect_outline light_blue 180 470 30 30 3;
+
+  rect_block pink 210 500 30 30;
+  rect_outline light_blue 210 500 30 30 3;
+
+  rect_block pink 240 470 30 30;
+  rect_outline light_blue 240 470 30 30 3;
+
+  rect_block pink 210 440 30 30;
+  rect_outline light_blue 210 440 30 30 3;
+
+  write_word black 30 290 480 "unnamed"
+
+let pentadecathlon () =
+  (* first half *)
+  rect_block pink 150 340 30 30;
+  rect_outline light_blue 150 340 30 30 3;
+  rect_block pink 180 340 30 30;
+  rect_outline light_blue 180 340 30 30 3;
+  rect_block pink 210 370 30 30;
+  rect_outline light_blue 210 370 30 30 3;
+  rect_block pink 210 310 30 30;
+  rect_outline light_blue 210 310 30 30 3;
+  rect_block pink 240 340 30 30;
+  rect_outline light_blue 240 340 30 30 3;
+  rect_block pink 270 340 30 30;
+  rect_outline light_blue 270 340 30 30 3;
+
+  (* second half *)
+  rect_block pink 300 340 30 30;
+  rect_outline light_blue 300 340 30 30 3;
+  rect_block pink 330 340 30 30;
+  rect_outline light_blue 330 340 30 30 3;
+  rect_block pink 360 370 30 30;
+  rect_outline light_blue 360 370 30 30 3;
+  rect_block pink 360 310 30 30;
+  rect_outline light_blue 360 310 30 30 3;
+  rect_block pink 390 340 30 30;
+  rect_outline light_blue 390 340 30 30 3;
+  rect_block pink 420 340 30 30;
+  rect_outline light_blue 420 340 30 30 3;
+
+  write_word black 30 470 350 "pentadecathlon"
+
+let figure_eight () =
+  (* lower *)
+  rect_block pink 420 470 30 30;
+  rect_outline light_blue 420 470 30 30 3;
+  rect_block pink 450 470 30 30;
+  rect_outline light_blue 450 470 30 30 3;
+  rect_block pink 390 470 30 30;
+  rect_outline light_blue 390 470 30 30 3;
+
+  rect_block pink 420 500 30 30;
+  rect_outline light_blue 420 500 30 30 3;
+  rect_block pink 450 500 30 30;
+  rect_outline light_blue 450 500 30 30 3;
+  rect_block pink 390 500 30 30;
+  rect_outline light_blue 390 500 30 30 3;
+
+  rect_block pink 420 530 30 30;
+  rect_outline light_blue 420 530 30 30 3;
+  rect_block pink 450 530 30 30;
+  rect_outline light_blue 450 530 30 30 3;
+  rect_block pink 390 530 30 30;
+  rect_outline light_blue 390 530 30 30 3;
+
+  (* upper *)
+  rect_block pink 510 560 30 30;
+  rect_outline light_blue 510 560 30 30 3;
+  rect_block pink 540 560 30 30;
+  rect_outline light_blue 540 560 30 30 3;
+  rect_block pink 480 560 30 30;
+  rect_outline light_blue 480 560 30 30 3;
+
+  rect_block pink 510 590 30 30;
+  rect_outline light_blue 510 590 30 30 3;
+  rect_block pink 540 590 30 30;
+  rect_outline light_blue 540 590 30 30 3;
+  rect_block pink 480 590 30 30;
+  rect_outline light_blue 480 590 30 30 3;
+
+  rect_block pink 510 620 30 30;
+  rect_outline light_blue 510 620 30 30 3;
+  rect_block pink 540 620 30 30;
+  rect_outline light_blue 540 620 30 30 3;
+  rect_block pink 480 620 30 30;
+  rect_outline light_blue 480 620 30 30 3;
+
+  write_word black 30 510 510 "figure eight"
+
+let acorn () =
+  (* actual pattern *)
+  rect_block pink 690 630 30 30;
+  rect_outline light_blue 690 630 30 30 3;
+  rect_block pink 690 600 30 30;
+  rect_outline light_blue 690 600 30 30 3;
+  rect_block pink 750 600 30 30;
+  rect_outline light_blue 750 600 30 30 3;
+  rect_block pink 720 540 30 30;
+  rect_outline light_blue 720 540 30 30 3;
+  rect_block pink 690 510 30 30;
+  rect_outline light_blue 690 510 30 30 3;
+  rect_block pink 690 480 30 30;
+  rect_outline light_blue 690 480 30 30 3;
+  rect_block pink 690 450 30 30;
+  rect_outline light_blue 690 450 30 30 3;
+
+  (* grid *)
+  rect_outline light_blue 720 600 30 30 3;
+  rect_outline light_blue 720 570 30 30 3;
+  rect_outline light_blue 690 570 30 30 3;
+  rect_outline light_blue 690 540 30 30 3;
+
+  write_word black 30 810 555 "acorn"
+
+let rec patterns_switch () =
+  let status = wait_next_event [ Key_pressed ] in
+  match status.key with
+  | '\t' -> patterns2_draw ()
+  | 'e' -> exit 0
+  | ' ' -> menu_draw ()
+  | _ -> patterns_switch ()
+
+and patterns2_switch () =
+  let status = wait_next_event [ Key_pressed ] in
+  match status.key with
+  | '\t' -> patterns_draw ()
+  | 'e' -> exit 0
+  | ' ' -> menu_draw ()
+  | _ -> patterns_switch ()
+
+and patterns_draw () =
   clear_graph ();
-  write_word black 50 410 700 "Patterns";
+  write_word black 50 480 700 "Patterns: Page 1";
 
   glider ();
   blinker ();
@@ -348,6 +516,22 @@ let patterns_draw () =
   light_spaceship ();
   block ();
 
-  write_word red 40 260 250 "Press 'e' to exit the game";
-  write_word red 40 120 170 "Press 'space' to go back to the main menu";
-  menu_switch ()
+  write_word red 40 432 250 "Press 'tab' to turn page";
+  write_word red 40 430 210 "Press 'e' to exit the game";
+  write_word red 40 390 170 "Press 'space' to go back to the main menu";
+  patterns_switch ()
+
+and patterns2_draw () =
+  clear_graph ();
+  write_word black 50 480 700 "Patterns: Page 2";
+
+  unnamed_1 ();
+  unnamed_2 ();
+  pentadecathlon ();
+  figure_eight ();
+  acorn ();
+
+  write_word red 40 432 250 "Press 'tab' to turn page";
+  write_word red 40 430 210 "Press 'e' to exit the game";
+  write_word red 40 390 170 "Press 'space' to go back to the main menu";
+  patterns2_switch ()
