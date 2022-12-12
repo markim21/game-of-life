5ff8389f55e0ae4b7f53be8ffe346a69471d1a75
Marya Kim <mnk45@cornell.edu>
2022-12-11 22:15:54 -0500

    Add shape live count tests

--- test/main.ml
+++ test/main.ml
@@ -4,6 +4,7 @@ open Game.Grid
 open Game.State
 open Game.Shapes
 
+(** BLACK BOX TESTING OF STATE.ML ************************************)
 let empty_grid = make_grid 3 3
 let grid_bread_start =
   {
@@ -247,15 +248,13 @@ let grid_C =
       |];
   }
 
-(** BLACK BOX TESTING OF STATE.ML *)
-
-(**[next_generation_test] tests new_generation. 
+(** [next_generation_test] tests new_generation. 
     a given grid [init_grid] after one iteration should match next_grid based on the rules of life. *)
 let next_generation_test (name : string) (init_grid : grid) (next_grid : grid) :
     test =
   name >:: fun _ -> assert_equal next_grid (new_generation init_grid)
 
-(**[generation_loop] returns a [grid] after [remaining_generations] generations*)
+(** [generation_loop] returns a [grid] after [remaining_generations] generations*)
 let rec generation_loop remaining_generations grid =
     match remaining_generations with
     | r when r <= 0 -> grid
@@ -270,12 +269,34 @@ let future_generations_test (name : string) (generations : int) (init_grid : gri
   assert_equal final_grid
     (generation_loop generations init_grid)
 
-(** BLACK BOX TESTING OF SHAPES.ML *)
+let future_generations_tests = [
+  future_generations_test "after 4 generations, a dead grid remains dead." 4
+      empty_grid empty_grid;
+  future_generations_test "after 50+ generations, a diamond never changes its shape"
+    100 grid_diamond grid_diamond;
+  future_generations_test "after 8 generations, a bread transposes" 
+    8 grid_bread_start grid_bread_end;
+  future_generations_test "after 10+ generations, a bread reaches the corner forever"
+    100 grid_bread_start grid_bread_corner;
+]
 
+let next_generation_tests =
+  [
+    next_generation_test "a dead grid cannot produce a living grid on its own"
+      empty_grid empty_grid;
+    next_generation_test "a 3x3 grid with [1,1] alive results in a dead grid"
+      grid_A empty_grid;
+    next_generation_test
+      "a 3x3 grid with [0,0] [1,1] [2,2] alive results in only [1,1] alive"
+      grid_B grid_A;
+  ]
+
+
+(** BLACK BOX TESTING OF SHAPES.ML **************************************)
+(* create a fresh empty grid *)
 let fresh_grid = 
   fun () -> 
     make_grid 50 50 
-
 
 (*A square block is static, and remains the same no matter what*)
 let square_block g = square_block g 25 25; g
@@ -299,9 +320,6 @@ let pulsar g = pulsar g 25 25; g
 
 (* Gosper Glider Gun emits a new glider ever 30 generations *)
 let gun g = gun g 0 4; g 
-
-(*let gun = gun grid 0 0; grid *)
-
 
 
 let shapes_oscillate_tests = [
@@ -397,13 +415,11 @@ let shapes_oscillate_tests = [
     "pentas are p-15 oscillators"
     60
     (penta (fresh_grid ()))
-    (penta (fresh_grid ()))
-     
+    (penta (fresh_grid ()))  
 ]
 
 
-let shapes_tests = 
-  shapes_oscillate_tests
+
 
 (** GLASS BOX TESTING OF STATE.ML *)
 
@@ -413,25 +429,20 @@ match squares_lst with
 | [] -> acc 
 | h :: t -> let lst = Array.to_list h in square_list_tr (lst@acc) (t)
 
-
 (**[square_array_to_lst] turns a square array array into square array list*)
 let square_array_to_lst (grid:grid) : square list = 
   let grid_lst = Array.to_list grid.squares in 
   square_list_tr [] grid_lst
     
-
 (**[grid_live_count_test] tests function get_live_count and count_living_tr. 
     counts the number of alive squares in a given grid *)
 let grid_live_count_test (name: string) (live_count : int) (grid: grid) : test = 
-  name >:: fun _ -> assert_equal live_count (
-    square_array_to_lst grid |> get_live_count )  
-
+  name >:: fun _ -> assert_equal live_count (square_array_to_lst grid |> get_live_count)  
 
 (**[generation_live_count_test] tests function count_living_tr,
     if the number of alive squares for a start generation and end generation are as predicted.*)
 let generation_live_count_test (name:string) (start_count : int) (init_grid : grid) (end_count : int ) (final_grid : grid) : test = 
   name >:: fun _ -> failwith "unimplemented"
-
 
 let ap1 (a : int) : int = a + 1
 let am1 (a : int) : int = a - 1
@@ -449,32 +460,10 @@ let coordinates =
     (ap1, ap1);
   ]
 
-
 let neighbors_test (name:string) (square:square) (grid:grid) (neighbors:square list) : test = 
   name >:: fun _ -> 
     assert_equal neighbors (get_neighbor_coordinates square grid coordinates [])
 
-let next_generation_tests =
-  [
-    next_generation_test "a dead grid cannot produce a living grid on its own"
-      empty_grid empty_grid;
-    next_generation_test "a 3x3 grid with [1,1] alive results in a dead grid"
-      grid_A empty_grid;
-    next_generation_test
-      "a 3x3 grid with [0,0] [1,1] [2,2] alive results in only [1,1] alive"
-      grid_B grid_A;
-  ]
-
-let future_generations_tests = [
-  future_generations_test "after 4 generations, a dead grid remains dead." 4
-      empty_grid empty_grid;
-  future_generations_test "after 50+ generations, a diamond never changes its shape"
-    100 grid_diamond grid_diamond;
-  future_generations_test "after 8 generations, a bread transposes" 
-    8 grid_bread_start grid_bread_end;
-  future_generations_test "after 10+ generations, a bread reaches the corner forever"
-    100 grid_bread_start grid_bread_corner;
-]
 
 let live_count_tests = [
   grid_live_count_test 
@@ -499,17 +488,56 @@ let live_count_tests = [
     grid_bread_start
 ]
 
+(** BLACK BOX TESTING SHAPES.ML OF THE SHAPES LIVE COUNT *)
+let shapes_live_tests = [
+  grid_live_count_test
+    "squares have 4 live squares"
+    4
+    (square_block (fresh_grid ()));
+  grid_live_count_test
+    "right gliders have 5 live squares"
+    5 
+    (right_glider (fresh_grid ()) 10 10);
+  grid_live_count_test
+    "left gliders have 5 live squares"
+    5 
+    (left_glider (fresh_grid ()) 10 10);
+  grid_live_count_test
+    "pulsars have 48 live squares"
+    48
+    (pulsar (fresh_grid ()));
+  grid_live_count_test
+    "pentadecathlons have 12 live squares"
+    12
+    (penta (fresh_grid ()));
+  grid_live_count_test
+    "LWSS have 9 live squares"
+    9
+    (l_spaceship (fresh_grid ()));
+  grid_live_count_test
+    "lines have 3 live squares"
+    3
+    (line (fresh_grid ()));
+  grid_live_count_test 
+    "stacks have 3 live squares"
+    3
+    (stack (fresh_grid ()));
+]
+
 let neighbors_tests = [
 
 ]
 
 
 let state_tests = 
-  next_generation_tests @
-  future_generations_tests @
-  live_count_tests @ 
-  neighbors_tests
+  next_generation_tests 
+  @ future_generations_tests 
+  @ live_count_tests 
+  @ neighbors_tests
 
+let shapes_tests = 
+  shapes_oscillate_tests
+  @ shapes_live_tests
 
 (** TEST GRID.ML **)
 
